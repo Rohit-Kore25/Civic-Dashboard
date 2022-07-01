064491a0a2f58931d1a5eafb824acd2f7d0e1511
rohit kore <81622814+Rohit-Kore25@users.noreply.github.com>
2022-07-01 18:53:55 +0530

    image insertion in database

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+require('dotenv/config');
 
 const app = express();
 
@@ -13,6 +17,34 @@ app.set('view engine','ejs');
 
 mongoose.connect("mongodb://localhost:27017/civicdashboardDB");
 
+const issueSchema = new mongoose.Schema({
+  Name:String,
+  Location:String,
+  Date:Date,
+  PhoneNum:Number,
+  ProblemType:String,
+  img:
+    {
+        data: Buffer,
+        contentType: String
+    },
+  IssueDesc:String,
+});
+
+const Issue = new mongoose.model('issue',issueSchema);
+
+const storage = multer.diskStorage({
+  destination:(req,file,cb) => {
+    cb(null,'uploads');
+  },
+  filename:(req,file,cb) => {
+    cb(null,file.fieldname + '-' + Date.now());
+  }
+});
+
+const upload = multer({storage:storage});
+
+//The gets:
 app.get("/areawisecomplaints",function(req,res){
   res.render("areacomplaints");
 });
@@ -23,6 +55,25 @@ app.get("/localcomplaints",function(req,res){
 
 app.get("/reportproblem",function(req,res){
   res.render("reportprob");
+});
+
+//The posts:
+app.post("/reportproblem",upload.single('image'),function(req,res){
+
+  const issueToAdd = new Issue({
+    Name:req.body.name,
+    Location:req.body.location,
+    Date:req.body.date,
+    PhoneNum:req.body.phonenum,
+    ProblemType:req.body.probtype,
+    img: {
+			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+			contentType: 'image/png'
+		},
+    IssueDesc:req.body.desc,
+  });
+
+  issueToAdd.save();
 });
 
 app.listen(process.env.PORT || 3000,function(){
